deb458bb26dda565711948434cf5896083da157e
meghamoree <meghamoree@gmail.com>
2020-04-29 20:00:53 +0530

    Add.Adding a use case 5 PassWord Rule1 to User registration test.

--- main/java/Registration.java
+++ main/java/Registration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
         String LastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
         String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
         String mobilePattern="[0-9]{2}[ ]{1}[0-9]{10}$";
+        String passwordPattern="^([a-zA-Z0-8]{8,})$";
         public boolean checkFirstName(String FirstName) {
             return Pattern.matches(FirstNamePattern, FirstName);
         }
@@ -17,9 +18,12 @@ import java.util.regex.Pattern;
         public boolean checkEmail(String email) {
             return Pattern.matches(emailPattern, email);
         }
-        
+
         public boolean checkMobileNumber(String mobileNumber) {
             return Pattern.matches(mobilePattern,mobileNumber);
         }
-
+        public boolean checkPassword(String password){
+            return  Pattern.matches(passwordPattern,password);
+        }
     }
+
