38a83ea94d2385ce92e30c8fbd4c2616b7e90191
meghamoree <meghamoree@gmail.com>
2020-04-29 17:13:37 +0530

    Add.Adding a LastName usecase 2 to UserRegistration Test.

--- test/java/RegistrationTest.java
+++ test/java/RegistrationTest.java
@@ -3,19 +3,33 @@ import org.junit.Test;
 
 
     public class RegistrationTest {
-        Registration obj = new Registration();
+        Registration registration = new Registration();
 
         @Test
         public void givenfirstName_whenValid_thenTrue() {
-            boolean firstname = obj.checkName("Megha");
-            Assert.assertTrue(firstname);
+            boolean result = registration.checkFirstName("Megha");
+            Assert.assertTrue(result);
         }
 
         @Test
         public void givenfirstName_whenInValid_thenFalse() {
-            boolean firstname = obj.checkName("Me");
-            Assert.assertFalse(firstname);
+            boolean result = registration.checkFirstName("Me");
+            Assert.assertFalse(result);
+        }
+
+        @Test
+        public void givenlastName_whenValid_thenTrue() {
+            boolean lastname = registration.checkLastName("More");
+
+            Assert.assertTrue(lastname);
+        }
+
+        @Test
+        public void givenlastName_wheninValid_thenFalse() {
+            boolean lastname = registration.checkLastName("Mo");
+            Assert.assertFalse(lastname);
         }
 
 
-}
+
+    }
