7d53dc8a76bb1ddf2be6c7239353bbead1c5c297
meghamoree <meghamoree@gmail.com>
2020-04-29 18:19:00 +0530

    refactor.modyfing main class for the tests.

--- main/java/Registration.java
+++ main/java/Registration.java
@@ -2,13 +2,19 @@
 import java.util.regex.Pattern;
 
     public class Registration {
-        String FirstNamePattern="^[A-Z][a-zA-Z]{2,}$";
-        String LastNamePattern="^[A-Z][a-zA-Z]{2,}$";
+        String FirstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+        String LastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+        String emailPattern="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
 
         public boolean checkFirstName(String FirstName) {
-            return Pattern.matches(FirstNamePattern,FirstName);
+            return Pattern.matches(FirstNamePattern, FirstName);
         }
-            public boolean checkLastName(String LastName) {
-                return Pattern.matches(LastNamePattern,LastName);
+
+        public boolean checkLastName(String LastName) {
+            return Pattern.matches(LastNamePattern, LastName);
         }
-}
+
+        public boolean checkEmail(String email) {
+            return Pattern.matches(emailPattern, email);
+        }
+    }
